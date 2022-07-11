303f59bee9431a30a2646bf9c80e733c43866d3f
hazhar ghaderi <hazhar24ghaderi@gmail.com>
2022-07-11 20:08:42 +0430

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './component/navbar'
+import Sidebar from './component/sidebar'
 
 export default function App() {
   return (
     <div>
       <Navbar />
+      <Sidebar />
     </div>
   )
 }
